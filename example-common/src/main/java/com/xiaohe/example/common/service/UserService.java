ae8e8fbcadac1fcd859b41eb84429949c2acd99d
xiaohe404 <xiaohe404@qq.com>
2024-03-09 17:24:04 +0800

    add 添加接口 Mock 服务

--- example-common/src/main/java/com/xiaohe/example/common/service/UserService.java
+++ example-common/src/main/java/com/xiaohe/example/common/service/UserService.java
@@ -14,4 +14,11 @@ public interface UserService {
      * @return
      */
     User getUser(User user);
+
+    /**
+     * 新方法 - 获取数字
+     */
+    default short getNumber() {
+        return 1;
+    }
 }
--- example-consumer/src/main/java/com/xiaohe/example/consumer/ConsumerExample.java
+++ example-consumer/src/main/java/com/xiaohe/example/consumer/ConsumerExample.java
@@ -16,5 +16,19 @@ public class ConsumerExample {
         RpcConfig rpc2 = ConfigUtils.loadConfigYml(RpcConfig.class, "rpc");
         System.out.println(rpc);
         System.out.println(rpc2);
+
+        // 获取代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("xiaohe");
+        // 调用
+        User newUser = userService.getUser(user);
+        if (newUser != null) {
+            System.out.println(newUser.getName());
+        } else {
+            System.out.println("user == null");
+        }
+        short number = userService.getNumber();
+        System.out.println(number);
     }
 }
