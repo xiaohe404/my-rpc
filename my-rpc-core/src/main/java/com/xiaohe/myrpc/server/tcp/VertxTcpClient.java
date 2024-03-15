74716077d66dd87b33dcedbef23e7d68c4cec103
xiaohe404 <xiaohe404@qq.com>
2024-03-15 16:02:21 +0800

    add 为框架添加 TCP 服务器

---
+++ my-rpc-core/src/main/java/com/xiaohe/myrpc/server/tcp/VertxTcpClient.java
@@ -0,0 +1,31 @@
+package com.xiaohe.myrpc.server.tcp;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.net.NetSocket;
+
+public class VertxTcpClient {
+
+    public void start() {
+        // 创建 Vert.x 实例
+        Vertx vertx = Vertx.vertx();
+
+        vertx.createNetClient().connect(8888, "localhost", result -> {
+            if (result.succeeded()) {
+                System.out.println("Connected to TCP server");
+                NetSocket socket = result.result();
+                // 发送数据
+                socket.write("Hello, server!");
+                // 接收响应
+                socket.handler(buffer -> {
+                    System.out.println("Received response from server: " + buffer.toString());
+                });
+            } else {
+                System.out.println("Failed to connect to TCP server");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new VertxTcpClient().start();
+    }
+}
